c6a049510b2c48f5cd88f8dab77d7900c641d8a7
ficar <ficar@DESKTOP-RS7CGLI.hsd1.nj.comcast.net>
2016-04-23 19:04:32 -0400

    needs more work on cases 2,3,4

--- src/Driver.java
+++ src/Driver.java
@@ -9,9 +9,9 @@ public class Driver {
 	//A-Z
 	public static void main(String[] args) throws IOException{
 		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-		HashTable<Integer, String> table = new HashTable<>();
+		HashTable<String, Integer> table = new HashTable<>();
 		char[] charVal = new char[6];
-		
+
 		while(true){
 			int selection = Integer.parseInt(read.readLine());
 			System.out.println("\t1. Insert a symbol in the symbol table");
@@ -20,54 +20,61 @@ public class Driver {
 			System.out.println("\t4. Display the hash index of a symbol");
 			System.out.println("\t5. Exit program");
 			System.out.println(">>Make a selection: " + selection);
-			
-			switch(selection){
-			
-			case 1:		
-					
-					System.out.println(">>Enter the a symbol consisting of 1-6 char: ");
-					String s = read.readLine();charVal = s.toCharArray();				
-					int iterations = s.length();int total = 0;int exp = s.length()-1;
-					
-														
-					for(int i = 0; i<iterations; i++){
-						System.out.println(charVal[i]);
-						int currentMapNumber = (char)charVal[i]-64;
-						System.out.println("Current number: " + currentMapNumber);
-						System.out.println("Current exp: " + i);
-						total += currentMapNumber * Math.pow(32,exp--);
-					}
-					System.out.println("Total: " + total);
-					System.out.println("Hash value: " + total%3);
-					table.tableInsert(total%3, s);
-					break;
-					
-			case 2:
-					System.out.println(">>Enter a symbol to delete: ");
-					String s1 = read.readLine();
-					break;
-					
-			case 3:
-					System.out.println("Retrieve and display a value in table: " );
-					String s2 = read.readLine();
-					break;
-			case 4:
-					System.out.println("Enter a symbol to return its hash index: ");
-					String s3 = read.readLine();
-					break;
-			
-			case 5:
-					System.out.println("Goodbye");
-					System.exit(0);
-					break;
-			
-			}
-			
-		}
-			
-			
 
- 
+			switch(selection){
+
+			case 1:		
+
+				System.out.println(">>Enter a symbol consisting of 1-6 char: ");
+				String s = read.readLine();charVal = s.toCharArray();				
+				int iterations = s.length();int total = 0;int exp = s.length()-1;																		
+				for(int i = 0; i<iterations; i++){					
+					int currentMapNumber = (char)charVal[i]-64;
+					total += currentMapNumber * Math.pow(32,exp--);
+				}
+				table.tableInsert(s,total%3);
+				break;
+
+			case 2:
+				System.out.println(">>Enter a symbol to delete: ");
+				String s1 = read.readLine();
+				boolean deleted = table.tableDelete(s1);
+				if(deleted){
+					System.out.println("Item " + s1 + " successfully deleted.");	
+				}
+				else{
+					System.out.println("Item " + s1 + " was NOT found.");	
+				}
+				break;
+
+			case 3:
+				System.out.println("Retrieve and display a value in table: " );
+				String s2 = read.readLine();
+				Integer val = table.tableRetrieve(s2);
+				if(val!=null){
+					System.out.println("The hash value for this symbol is: " + val);
+				}
+				else{
+					System.out.println("Does not exist.");
+				}
+				break;
+			case 4:
+				System.out.println("Enter a symbol to return its hash index: ");
+				String s3 = read.readLine();
+				break;
+
+			case 5:
+				System.out.println("Goodbye");
+				System.exit(0);
+				break;
+
+			}
+
+		}
+
+
+
+
 
 
 	}
--- src/HashTable.java
+++ src/HashTable.java
@@ -5,60 +5,87 @@
 // *********************************************************
 
 public class HashTable<K, V> implements HashTableInterface<K,V> {
-  private ChainNode[] table;     // hash table
-  private int size = 0;          // size of ADT table
+	private ChainNode[] table;     // hash table
+	private int size = 0;          // size of ADT table
 
-  public HashTable() {
-    table = new ChainNode[3];
-  }  // end default constructor
+	public HashTable() {
+		table = new ChainNode[3];
+	}  // end default constructor
 
-// table operations
-  public boolean tableIsEmpty() {
-    return size==0;
-  }  // end tableIsEmpty
+	// table operations
+	public boolean tableIsEmpty() {
+		return size==0;
+	}  // end tableIsEmpty
 
-  public int tableLength() {
-    return size;
-  }  // end tableLength
-  
-  public void tableInsert(K key, V value) throws HashException{
-	  if(table[(Integer)key]==null){
-		  table[(Integer)key] = new ChainNode(key, value, null);
-	  }
-	  else{
-		  ChainNode<K, V> newChainNode = new ChainNode(key, value, table[(Integer)key]);
-	  }  
-	  size++;
-  }
-  
-  public boolean tableDelete(K searchKey){
-	  for(int i = 0; i<size; i++){
-		  if(table[i].getKey()==searchKey){
-			  table[i-1].setNext(table[i+1]);
-			  size--;
-			  return true;
-		  }
-	  }
-	  return false;
-  }
-  
-  public V tableRetrieve(K searchKey){
-	  for(int i = 0; i<size; i++){
-		  if(table[i].getKey()==searchKey){
-			  return (V)table[i];
-		  }
-	  }
-	  return null;
-  }
-  
-  public int hashIndex(K key){
-	  for(int i = 0; i<size; i++){
-		  if(table[i].getKey()==key){
-			  return i;
-		  }
-	  }
-	  return -1;
-  }
+	public int tableLength() {
+		return size;
+	}  // end tableLength
+
+	public void tableInsert(K key, V value) throws HashException{
+
+		if(table[(Integer)value]==null){
+			table[(Integer)value] = new ChainNode(key, value, null);
+		}
+		else{
+			ChainNode<K, V> tail = table[(Integer)value];
+			ChainNode<K, V> newChainNode = new ChainNode(key, value, null);
+			while(tail.getNext()!=null){
+				tail = tail.getNext();
+			}
+			tail.setNext(newChainNode);		  
+		}  
+		size++;
+	}
+
+	public boolean tableDelete(K searchKey){
+		if(size>0){
+			for(int i = 0; i<3; i++){
+				if(table[i]!=null){
+					ChainNode<K, V> current = table[i];
+					if(current.getNext()==null){
+						current = null;
+						return true;
+					}
+					while(current!=null){						
+						if(current.getNext().getKey().equals(searchKey)){
+							current.setNext(current.getNext().getNext());
+							size--;
+							return true;
+						}
+						current = current.getNext();
+					}
+				}
+			}
+		}	
+		return false;
+	}
+
+	public V tableRetrieve(K searchKey){
+		if(size>0){
+			for(int i = 0; i<3; i++){
+				if(table[i]!=null){
+					ChainNode<K, V> current = table[i];
+					while(current.getNext()!=null){		  
+						if(current.getNext().getKey().equals(searchKey)){													
+							return current.getNext().getValue();
+						}
+						current = current.getNext();
+					}
+				}
+			}
+		}	
+		return null;
+	}
+
+
+	public int hashIndex(K key){
+		for(int i = 0; i<size; i++){
+			if(table[i].getKey()==key){
+				return i;
+			}
+		}
+		return -1;
+	}
 
 
 }  // end HashTable
